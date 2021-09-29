640a8e26a09411cecd0813050f39226f332be7d3
HeddensL <lhedde1@live.spcollege.edu>
2021-09-28 22:02:34 -0400

    Created StopwatchTracker class

---
+++ MyFitTimer/Server/StopwatchTracker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFitTimer.Server
+{
+    public class StopwatchTracker
+    {
+    }
+}
