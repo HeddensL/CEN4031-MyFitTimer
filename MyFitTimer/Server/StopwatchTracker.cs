519cf45243ee0cff7cf0188bfaf8f3f7627fbd6f
HeddensL <lhedde1@live.spcollege.edu>
2021-09-29 19:02:45 -0400

    basic functionality for StopwatchTracker class. Unit tests for StopwatchTracker methods.

--- MyFitTimer/Server/StopwatchTracker.cs
+++ MyFitTimer/Server/StopwatchTracker.cs
@@ -8,15 +8,34 @@ namespace MyFitTimer.Server
 {
     public class StopwatchTracker
     {
+        //return currently elapsed time
         public TimeSpan GetElapsed()
         {
+            return timer.Elapsed;
+        }
+
+        //returns previous lap
+        public TimeSpan GetLap()
+        {
             return lap;
+        }
+
+        public void Restart()
+        {
+            timer.Reset();
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            lap = timer.Elapsed;
         }
 
         //creates new stopwatch object
         private Stopwatch timer = new Stopwatch();
 
-        //creates new timespan object
+        //creates new timespan object to hold time
         private TimeSpan lap = new TimeSpan();
     }
 }
