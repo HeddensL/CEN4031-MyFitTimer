a3d6c5613b92eef912f79236e47984a84b3175f5
HeddensL <lhedde1@live.spcollege.edu>
2021-10-05 23:00:21 -0400

    additional logic layer

---
+++ MyFitTimer/Server/Controllers/StopwatcherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyFitTimer.Server.Data;
+using MyFitTimer.Shared;
+using MyFitTimer.Server.Services;
+
+namespace MyFitTimer.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StopwatcherController : ControllerBase
+    {       
+        private readonly ITimeKeeperDataService _timeKeeperDataService;
+
+        public StopwatcherController(ITimeKeeperDataService timeKeeperDataService)
+        {
+            _timeKeeperDataService = timeKeeperDataService;
+        }        
+
+        [HttpGet]
+        public async Task<IActionResult> GetLatestTime()
+        {                     
+            return Ok(await _timeKeeperDataService.GetLatestTimeKeepers());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTime(TimeKeeper time)
+        {
+            return Ok(await _timeKeeperDataService.CreateTimeKeepers(time));
+        }
+    }
+}
