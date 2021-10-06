1f52482ec9ff98cf6333d96f0f3cdb53abc018e4
Sarah Valle <svalle6@live.spcollege.edu>
2021-10-05 22:54:54 -0400

    Presentation Layer

---
+++ MyFitTimer/Client/Services/ITimeKeeperService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyFitTimer.Shared;
+
+namespace MyFitTimer.Client.Services
+{
+    public interface ITimeKeeperService
+    {
+        event Action OnChange;
+
+        List<TimeKeeper> Times { get; set; }
+
+        Task<List<TimeKeeper>> GetTimeKeepers();
+
+        Task<List<TimeKeeper>> CreateTimeKeeper(TimeKeeper time);
+
+        Task<List<TimeKeeper>> DeleteTimeKeeper(int id);
+    }
+}
---
+++ MyFitTimer/Client/Services/TimeKeeperService.cs
@@ -0,0 +1,48 @@
+﻿using MyFitTimer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace MyFitTimer.Client.Services
+{
+    public class TimeKeeperService : ITimeKeeperService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TimeKeeperService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public event Action OnChange;
+
+        public List<TimeKeeper> Times { get; set; } = new List<TimeKeeper>();
+
+        public async Task<List<TimeKeeper>> CreateTimeKeeper(TimeKeeper time)
+        {
+            var result = await _httpClient.PostAsJsonAsync<TimeKeeper>($"api/timekeeper", time);
+            Times = await result.Content.ReadFromJsonAsync<List<TimeKeeper>>();
+            OnChange.Invoke();
+            return Times;
+        }
+
+        public async Task<List<TimeKeeper>> GetTimeKeepers()
+        {
+            Times = await _httpClient.GetFromJsonAsync<List<TimeKeeper>>("api/timekeeper");
+            return Times;
+        }
+
+        public async Task<List<TimeKeeper>> DeleteTimeKeeper(int id)
+        {
+            var result = await _httpClient.DeleteAsync($"api/timekeeper/{id}");
+            Times = await result.Content.ReadFromJsonAsync<List<TimeKeeper>>();
+            OnChange.Invoke();
+            return Times;
+        }
+    }
+
+    
+}
